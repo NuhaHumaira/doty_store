0bf984c2f5018481c13a672a7843d210cd53ecec
NuhaHumaira <Nuhnaznoy@gmail.com>
2025-07-14 10:26:47 +0800

    Create path_01.sh

---
+++ path_01.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "[" > store.json
+first=true
+
+for dir in app/*/; do
+  id=$(basename "$dir")
+  name=$(echo "$id" | sed -e 's/^./\U&/' -e 's/-/ /g')
+  avatar_file=$(find "$dir" -iregex ".*\.\(jpg\|png\|jpeg\)" | head -n 1)
+  ipk_file=$(find "$dir" -name "*.ipk" | head -n 1)
+
+  [ -z "$ipk_file" ] && continue
+
+  filename=$(basename "$ipk_file")
+  package=$(echo "$filename" | cut -d_ -f1)
+  version=$(echo "$filename" | cut -d_ -f2)
+  avatar_url="https://raw.githubusercontent.com/${GITHUB_REPOSITORY}/main/${avatar_file}"
+  ipk_url="https://raw.githubusercontent.com/${GITHUB_REPOSITORY}/main/${ipk_file}"
+
+  if [ "$first" = true ]; then
+    first=false
+  else
+    echo "," >> store.json
+  fi
+
+  cat <<EOF >> store.json
+{
+  "id": "$id",
+  "name": "$name",
+  "package": "$package",
+  "version": "$version",
+  "avatar": "$avatar_url",
+  "url": "$ipk_url"
+}
+EOF
+
+done
+
+echo "]" >> store.json
