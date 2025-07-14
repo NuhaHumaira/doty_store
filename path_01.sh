c1b1b6cb50f29aacb0cc70839db3f13b7e3237ea
NuhaHumaira <Nuhnaznoy@gmail.com>
2025-07-14 10:33:08 +0800

    Update path_01.sh

--- path_01.sh
+++ path_01.sh
@@ -6,14 +6,28 @@ first=true
 for dir in app/*/; do
   id=$(basename "$dir")
   name=$(echo "$id" | sed -e 's/^./\U&/' -e 's/-/ /g')
-  avatar_file=$(find "$dir" -iregex ".*\.\(jpg\|png\|jpeg\)" | head -n 1)
+  avatar_file=$(find "$dir" -iregex ".*\.\(jpg\|jpeg\|png\)" | head -n 1)
   ipk_file=$(find "$dir" -name "*.ipk" | head -n 1)
 
   [ -z "$ipk_file" ] && continue
 
   filename=$(basename "$ipk_file")
-  package=$(echo "$filename" | cut -d_ -f1)
-  version=$(echo "$filename" | cut -d_ -f2)
+
+  # Parse package and version
+  if [[ "$filename" == *_*_* ]]; then
+    package=$(echo "$filename" | cut -d_ -f1)
+    version=$(echo "$filename" | cut -d_ -f2)
+  else
+    base=$(basename "$filename" .ipk)
+    package=$(echo "$base" | sed -E 's/(.*)-[^-]+$/\1/')
+    version=$(echo "$base" | sed -E 's/.*-([^-]+)$/\1/')
+  fi
+
+  # Add "v" prefix to version if missing
+  if [[ ! "$version" =~ ^v ]]; then
+    version="v$version"
+  fi
+
   avatar_url="https://raw.githubusercontent.com/${GITHUB_REPOSITORY}/main/${avatar_file}"
   ipk_url="https://raw.githubusercontent.com/${GITHUB_REPOSITORY}/main/${ipk_file}"
 
